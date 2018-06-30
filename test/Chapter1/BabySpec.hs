7cd4f06fb53ed10d92b2e9f3e252d7cf5425fe2a
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-06-30 14:49:55 +0900

    Add 1.2

--- test/Chapter1/BabySpec.hs
+++ test/Chapter1/BabySpec.hs
@@ -11,4 +11,17 @@ spec = do
   describe "doubleMe" $ do
     it "Success" $
       doubleMe 2 `shouldBe` 4
-
+  describe "doubleUs" $ do
+    it "Success" $
+      doubleUs 2 3 `shouldBe` 10
+  describe "doubleSmallNumber" $ do
+    it "Success" $
+      doubleSmallNumber 2 `shouldBe` 4
+    it "Success2" $
+      doubleSmallNumber 101 `shouldBe` 101
+  describe "doubleSmallNumber'" $ do
+    it "Success" $
+      doubleSmallNumber' 2 `shouldBe` 5
+  describe "conanO'Brien" $ do
+    it "Success" $
+      conanO'Brien `shouldBe` "It's a=me, Conan O'Brien!"
