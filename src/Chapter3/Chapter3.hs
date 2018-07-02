8c1fe50f435c0c9eef56573b8396d3062ad99108
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-02 22:38:15 +0900

    Add 3.1 パターンマッチ

--- src/Chapter3/Chapter3.hs
+++ src/Chapter3/Chapter3.hs
@@ -1,1 +1,51 @@
 module Chapter3.Chapter3 where
+
+lucky :: Int -> String
+lucky 7 = "Is 7"
+lucky x = "Is not 7"
+
+sayMe :: Int -> String
+sayMe 1 = "One"
+sayMe 2 = "Two"
+sayMe 3 = "Three"
+sayMe x = "Not between 1 and 3"
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial(n - 1)
+
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Broseph"
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+listMatch :: [(Int, Int)] -> [Int]
+listMatch xs = [a + b | (a, b) <- xs]
+
+head' :: [a] -> a
+head' [] = error "Empty list"
+head' (x : _) = x
+
+tell :: [a] -> String
+tell [] = "Empty"
+tell (x : []) = "One"
+tell (x : y : []) = "Two"
+tell (x : y : _) = "Long"
+
+badAdd :: (Num a) => [a] -> a
+badAdd (x : y : z : []) = x + y + z
+
+firstLetter :: String -> String
+firstLetter "" = "Empty"
+firstLetter all@(x : xs) = all ++ " is " ++ [x]
--- test/Chapter3/Chapter3Spec.hs
+++ test/Chapter3/Chapter3Spec.hs
@@ -1,6 +1,7 @@
 module Chapter3.Chapter3Spec (spec) where
 
 import Test.Hspec
+import Control.Exception (evaluate)
 import Chapter3.Chapter3
 
 main :: IO ()
@@ -8,4 +9,50 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "Type" $ do
+  describe "Pattern match" $ do
+    it "lucky" $
+      lucky 7 `shouldBe` "Is 7"
+    it "lucky2" $
+      lucky 1 `shouldBe` "Is not 7"
+    it "sayMe" $
+      sayMe 2 `shouldBe` "Two"
+    it "sayMe2" $
+      sayMe 4 `shouldBe` "Not between 1 and 3"
+    it "factorial" $
+      factorial 0 `shouldBe` 1
+    it "factorial2" $
+      factorial 3 `shouldBe` 1 * 2 * 3
+    it "charName" $
+      charName 'a' `shouldBe` "Albert"
+    it "charName2" $
+      evaluate (charName 'c') `shouldThrow` anyException
+    it "addVectors" $
+      addVectors (1, 2) (3, 4) `shouldBe` (4, 6)
+    it "first" $
+      first (1, 2, 3) `shouldBe` 1
+    it "second" $
+      second (1, 2, 3) `shouldBe` 2
+    it "third" $
+      third (1, 2, 3) `shouldBe` 3
+    it "listMatch" $
+      listMatch [(1, 2), (3, 4)] `shouldBe` [3, 7]
+    it "head'" $
+      head [1, 2, 3] `shouldBe` 1
+    it "head'2" $
+      head "Hello" `shouldBe` 'H'
+    it "head'3" $
+      evaluate (head' []) `shouldThrow` anyException
+    it "tell" $
+      tell [] `shouldBe` "Empty"
+    it "tell2" $
+      tell "XY" `shouldBe` "Two"
+    it "tell3" $
+      tell "Hello" `shouldBe` "Long"
+    it "badAdd" $
+      badAdd [1, 2, 3] `shouldBe` 6
+    it "badAdd2" $
+      evaluate (badAdd [1, 2]) `shouldThrow` anyException
+    it "firstLetter" $
+      firstLetter "" `shouldBe` "Empty"
+    it "firstLetter" $
+      firstLetter "Hello" `shouldBe` "Hello is H"
