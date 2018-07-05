f806176ff75c8fa1c7a10969bb44c36849d51cb6
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-05 20:02:54 +0900

    Add 5.2 高階実演

--- src/Chapter5/Chapter5.hs
+++ src/Chapter5/Chapter5.hs
@@ -20,3 +20,18 @@ isUpperAlphanum = (`elem` ['A'..'Z'])
 
 minusFour :: Int -> Int
 minusFour = subtract 4
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+  where g x y = f y x
+
+flip'' :: (a -> b -> c) -> (b -> a -> c)
+flip'' f y x = f x y
--- test/Chapter5/Chapter5Spec.hs
+++ test/Chapter5/Chapter5Spec.hs
@@ -24,3 +24,28 @@ spec = do
       isUpperAlphanum 'a' `shouldBe` False
     it "minusFour" $
       minusFour 3 `shouldBe` -1
+    it "applyTwice" $
+      applyTwice (+3) 10 `shouldBe` 16
+    it "applyTwice2" $
+      applyTwice (multThree 2 2) 9 `shouldBe` 144
+    it "applyTwice3" $
+      applyTwice (3 :) [1] `shouldBe` [3, 3, 1]
+    it "zipWith'" $
+      zipWith' (+) [1, 2, 3] [4, 5, 6] `shouldBe` [5, 7, 9]
+    it "zipWith'2" $
+      zipWith' max [1, 2, 3] [4, 5, 6] `shouldBe` [4, 5, 6]
+    it "zipWith'3" $
+      zipWith' (*) (replicate 5 2) [1..] `shouldBe` [2, 4, 6, 8, 10]
+    it "zipWith'4" $
+      zipWith' (zipWith' (*)) [[1, 2, 3], [4, 5, 6]] [[7, 8, 9], [0, 1, 2]] `shouldBe`
+      [[7, 16, 27], [0, 5, 12]]
+    it "zip" $
+      zip [1, 2, 3] "abc" `shouldBe` [(1, 'a'), (2, 'b'), (3, 'c')]
+    it "flip'" $
+      flip' zip [1, 2, 3] "abc" `shouldBe` [('a', 1), ('b', 2), ('c', 3)]
+    it "flip'2" $
+      flip' zip [1, 2, 3, 4, 5] "hello" `shouldBe` flip'' zip [1, 2, 3, 4, 5] "hello"
+    it "zipWith with div" $
+      zipWith div [2, 2..] [10, 8, 6, 4, 2] `shouldBe` [0, 0, 0, 0, 1]
+    it "zipWith with flip div" $
+      zipWith (flip' div) [2, 2..] [10, 8, 6, 4, 2] `shouldBe` [5, 4, 3, 2, 1]
