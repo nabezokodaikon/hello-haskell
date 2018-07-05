56a720525da4e824cf354d2655c490dac75ab06a
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-05 10:12:03 +0900

    Add 4.2 さらにいくつかの再帰関数

--- src/Chapter4/Chapter4.hs
+++ src/Chapter4/Chapter4.hs
@@ -4,3 +4,32 @@ maximum' :: (Ord a) => [a] -> a
 maximum' [] = error "maximum of empty list!"
 maximum' [x] = x
 maximum' (x : xs) = max x (maximum' xs)
+
+replicate' :: Int -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x : replicate' (n - 1) x
+
+take' :: Int -> [a] -> [a]
+take' n _ 
+    | n <= 0 = []
+take' _ [] = []
+take' n (h : t) = h : take' (n - 1) t
+
+reverse' :: (Ord a) => [a] -> [a]
+reverse' [] = []
+reverse' (h : t) = reverse' t ++ [h]
+
+repeat' :: a -> [a]
+repeat' a = a : repeat' a
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (a : as) (b : bs) = (a, b) : zip' as bs
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x : xs)
+  | a == x = True
+  | otherwise = a `elem` xs
--- test/Chapter4/Chapter4Spec.hs
+++ test/Chapter4/Chapter4Spec.hs
@@ -18,3 +18,31 @@ spec = do
     it "maximum'3" $
       evaluate (maximum' []) `shouldThrow` anyException
     -}
+    it "replicate'" $
+      replicate' 3 "abc" `shouldBe` ["abc", "abc", "abc"]
+    it "replicate'2" $
+      replicate' 0 "abc" `shouldBe` []
+    it "take'" $
+      take 3 [5, 4, 3, 2, 1] `shouldBe` [5, 4, 3]
+    it "reverse'" $
+      reverse [1, 2, 3] `shouldBe` [3, 2, 1]
+    it "repeat'" $
+      take' 3 (repeat' 5) `shouldBe` [5, 5, 5]
+    {- TODO: 空リストの比較。
+    it "zip'" $
+      zip' [] [1, 2, 3] `shouldBe` []
+    it "zip'2" $
+      zip' [1, 2, 3] [] `shouldBe` []
+    -}
+    it "zip'3" $
+      zip' [1, 2, 3] [4, 5, 6] `shouldBe` [(1, 4), (2, 5), (3, 6)]
+    it "zip'4" $
+      zip' [1, 2, 3, 4] [4, 5, 6] `shouldBe` [(1, 4), (2, 5), (3, 6)]
+    it "zip'5" $
+      zip' [1, 2, 3] [4, 5, 6, 7] `shouldBe` [(1, 4), (2, 5), (3, 6)]
+    it "elem'" $
+      elem 1 [] `shouldBe` False
+    it "elem'2" $
+      elem 1 [2, 3] `shouldBe` False
+    it "elem'3" $
+      elem 3 [2, 3] `shouldBe` True
