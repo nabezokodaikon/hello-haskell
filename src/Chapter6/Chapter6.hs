e2be6bbefdd7c8981a47e7b1835b0a38e93a7624
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-09 07:27:14 +0900

    Add 6.1 モジュールをインポートする。

--- src/Chapter6/Chapter6.hs
+++ src/Chapter6/Chapter6.hs
@@ -1,1 +1,13 @@
 module Chapter6.Chapter6 where
+
+{- import Data.List -}
+{- import Data.List (nub, sort) -}
+{- import qualified Data.List (nub) -}
+import qualified Data.List as L (nub)
+
+{- ポイントフリースタイル -}
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . L.nub
+
+numUniques' :: (Eq a) => [a] -> Int
+numUniques' = \xs -> length (L.nub xs)
--- test/Chapter6/Chapter6Spec.hs
+++ test/Chapter6/Chapter6Spec.hs
@@ -10,3 +10,7 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Chapter6" $ do
+    it "numUniques" $
+      numUniques [1, 1, 2, 3] `shouldBe` 3
+    it "numUniques'" $
+      numUniques [1, 1, 2, 3] `shouldBe` numUniques' [1, 1, 2, 3]
