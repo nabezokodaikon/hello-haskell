e6b5831d35885d3be68f75574d8aae905d4450b9
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-13 07:13:40 +0900

    Add 7.2 形づくる

--- src/Chapter7/Chapter7.hs
+++ src/Chapter7/Chapter7.hs
@@ -1,1 +1,9 @@
 module Chapter7.Chapter7 where
+
+data Shape = Circle Float Float Float |
+             Rectangle Float Float Float Float
+  deriving (Show, Eq)
+
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r ^ 2
+area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
--- test/Chapter7/Chapter7Spec.hs
+++ test/Chapter7/Chapter7Spec.hs
@@ -1,5 +1,6 @@
 module Chapter7.Chapter7Spec (spec) where
 
+import Test.Hspec
 import Chapter7.Chapter7
 
 main :: IO ()
@@ -8,3 +9,10 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Chapter7" $ do
+    it "area of Circle" $
+      (area $ Circle 10 20 10) `shouldBe` 314.15927
+    it "area Of Rectangle" $
+      (area $ Rectangle 0 0 10 10) `shouldBe` 100
+    it "map and curry" $
+      map (Circle 10 20) [1, 2] `shouldBe` [Circle 10 20 1, Circle 10 20 2]
+      {- map (\a -> Circle 10 20 a) [1, 2] `shouldBe` [Circle 10 20 1, Circle 10 20 2] -}
