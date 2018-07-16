067942f8d1d57f5ce3714090236d7d6cdc6d6475
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-16 19:29:20 +0900

    Add 7.7 再帰的なデータ構造

--- src/Chapter7/Chapter7_7.hs
+++ src/Chapter7/Chapter7_7.hs
@@ -1,1 +1,6 @@
 module Chapter7.Chapter7_7 where
+
+data MyList a = Empty | Cons a (MyList a) deriving (Show, Read, Eq, Ord)
+{- 上と同義 -}
+data MyList' b = Empty' | Cons' { listhead :: b, listTail :: MyList' b }
+  deriving (Show, Read, Eq, Ord)
--- test/Chapter7/Chapter7_7Spec.hs
+++ test/Chapter7/Chapter7_7Spec.hs
@@ -9,3 +9,15 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Chapter7_7" $ do
+    it "List Example" $
+      [5] `shouldBe` 5:[]
+    it "List Example2" $
+      [4, 5] `shouldBe` 4:(5:[])
+    it "List Example3" $
+      [3, 4, 5] `shouldBe` 3:(4:(5:[]))
+    it "List Example4" $
+      [3, 4, 5] `shouldBe` 3:4:5:[]
+    it "MyList" $
+      5 `Cons` Empty `shouldBe` Cons 5 Empty
+    it "MyList2" $
+      4 `Cons` (5 `Cons` Empty) `shouldBe` Cons 4 (Cons 5 Empty)
