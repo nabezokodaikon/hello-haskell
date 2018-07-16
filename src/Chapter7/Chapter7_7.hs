c45e0f0a00b9a4f65c3a18352a8a1a7d6910b851
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-17 07:08:04 +0900

    Add 7.7 木を植えよう

--- src/Chapter7/Chapter7_7.hs
+++ src/Chapter7/Chapter7_7.hs
@@ -13,3 +13,26 @@ infixr ^++
 (^++) :: List a -> List a -> List a
 Empty ^++ ys = ys
 (x :-: xs) ^++ ys = x :-: (xs ^++ ys) -- 値コンストラクタをマッチさせる。(パターンマッチ)
+
+{- 木を植えよう -}
+data Tree a = EmptyTree | Node a (Tree a) (Tree a)
+  deriving (Show, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x -- xを唯一の要素として持つ木を挿入する。
+treeInsert x (Node a left right)
+  | x == a = Node x left right -- 新しい要素とルート要素が等しければ、元の木をそのまま返す。
+  | x < a = Node a (treeInsert x left) right -- 新しい要素が挿入された左部分木を持つ木を返す。
+  | x > a = Node a left (treeInsert x right) -- 新しい要素が挿入された右部分木を持つ木を返す。
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
+
+myTree = foldr treeInsert EmptyTree [8, 6, 4, 1, 7, 3, 5]
--- test/Chapter7/Chapter7_7Spec.hs
+++ test/Chapter7/Chapter7_7Spec.hs
@@ -26,4 +26,18 @@ spec = do
       3 :-: 4 :-: 5 :-: Empty `shouldBe` 3 :-: (4 :-: (5 :-: Empty))
     it "^++" $
       (3 :-: 4 :-: Empty) ^++ (5 :-: 6 :-: Empty) `shouldBe` 3 :-: (4 :-: (5 :-: (6 :-: Empty)))
-
+  describe "木を植えよう" $ do
+    it "myTree" $
+      myTree `shouldBe` Node 5
+                          (Node 3
+                            (Node 1 EmptyTree EmptyTree)
+                            (Node 4 EmptyTree EmptyTree) 
+                          )
+                          (Node 7
+                            (Node 6 EmptyTree EmptyTree)
+                            (Node 8 EmptyTree EmptyTree) 
+                          )
+    it "treeElem" $
+      8 `treeElem` myTree `shouldBe` True
+    it "treeElem 2" $
+      10 `treeElem` myTree `shouldBe` False
