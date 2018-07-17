aaa1cd86d7e6680270018d66b083465bb8d4dba0
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-17 16:45:22 +0900

    Add 7.8 交通信号データ型

--- src/Chapter7/Chapter7_8.hs
+++ src/Chapter7/Chapter7_8.hs
@@ -1,1 +1,23 @@
 module Chapter7.Chapter7_8 where
+
+{-
+Eq型クラス
+class Eq a where
+  (==) :: a -> a -> Bool
+  (/=) :: a -> a -> Bool
+  x == y = not (x /= y)
+  x /= y = not (x == y)
+-}
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+  Red == Red = True
+  Yellow == Yellow = True
+  Green == Green = True
+  _ == _ = False
+
+instance Show TrafficLight where
+  show Red = "Red light"
+  show Yellow = "Yellow light"
+  show Green = "Green light"
--- test/Chapter7/Chapter7_8Spec.hs
+++ test/Chapter7/Chapter7_8Spec.hs
@@ -8,4 +8,14 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "Chapter7_8 型クラス 中級講座" $ do
+  describe "Chapter7_8 交通信号データ型" $ do
+    it "Eq" $
+      Red == Red `shouldBe` True
+    it "Eq 2" $
+      Red == Yellow `shouldBe` False
+    it "elem" $
+      Red `elem` [Red, Yellow, Green] `shouldBe` True
+    it "show" $
+      show Red `shouldBe` "Red light"
+    {- it "show 2" $ -}
+      {- [Red, Yellow, Green] `shouldBe` ["Red light", "Yellow light", "Green light"] -}
