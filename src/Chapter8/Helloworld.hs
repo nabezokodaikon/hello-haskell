33eb24d3e6c4b727706d16f80cfeb91651ac3543
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-19 07:59:58 +0900

    Add 8.3 I/Oアクションどうしをまとめる

--- src/Chapter8/Helloworld.hs
+++ src/Chapter8/Helloworld.hs
@@ -1,3 +1,13 @@
 module Chapter8.Helloworld where
 
+{-
+-- 8.2
 main = putStrLn "hello, world"
+-}
+
+-- 8.3
+main :: IO ()
+main = do
+  putStrLn "Hello, what's your name?"
+  name <- getLine
+  putStrLn ("Hey " ++ name ++ ", you rock!")
