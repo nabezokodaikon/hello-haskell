eb5fb0c835f42b9b67bb4c59f2e60e954b2b6129
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-18 21:50:43 +0900

    Clean qualified name

--- test/Chapter8/Chapter8Spec.hs
+++ test/Chapter8/Chapter8Spec.hs
@@ -2,7 +2,7 @@ module Chapter8.Chapter8Spec (spec) where
 
 import Test.Hspec
 import Chapter8.Chapter8
-import qualified Chapter8.Helloworld as Helloworld 
+import qualified Chapter8.Helloworld as HW 
 
 main :: IO ()
 main = hspec spec
@@ -11,4 +11,4 @@ spec :: Spec
 spec = do
   describe "8.2 Hello, World!" $ do
     it "Hello, World!" $
-      Helloworld.main `shouldReturn` ()
+      HW.main `shouldReturn` ()
