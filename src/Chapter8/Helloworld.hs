88f0586965fbf810c139b2aa5f0780512498c37b
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-18 17:07:42 +0900

    Add 8.2 Hello, World!

---
+++ src/Chapter8/Helloworld.hs
@@ -0,0 +1,3 @@
+module Chapter8.Helloworld where
+
+main = putStrLn "hello, world"
--- test/Chapter8/Chapter8Spec.hs
+++ test/Chapter8/Chapter8Spec.hs
@@ -2,9 +2,13 @@ module Chapter8.Chapter8Spec (spec) where
 
 import Test.Hspec
 import Chapter8.Chapter8
+import qualified Chapter8.Helloworld as Helloworld 
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "8.2 Hello, World!" $ do
+    it "Hello, World!" $
+      Helloworld.main `shouldReturn` ()
