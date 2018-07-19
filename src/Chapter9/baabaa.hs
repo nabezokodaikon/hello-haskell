eef7e88fae0803b65109508718df243900f6ed0e
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-20 06:36:15 +0900

    Add 9.2 withFile関数を使う

--- src/Chapter9/baabaa.hs
+++ src/Chapter9/baabaa.hs
@@ -2,9 +2,16 @@
 
 import System.IO
 
+{-
 main :: IO ()
 main = do
   handle <- openFile "src/Chapter9/baabaa.txt" ReadMode
   contents <- hGetContents handle
   putStr contents
   hClose handle
+-}
+
+main :: IO ()
+main = withFile "src/Chapter9/baabaa.txt" ReadMode $ \handle -> do
+  contents <- hGetContents handle
+  putStr contents
