c9c65320617f61a9be6585fa00c3064826f90820
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-21 17:21:42 +0900

    Add 9.3 アイテムの削除

---
+++ src/Chapter9/DeleteToDo.hs
@@ -0,0 +1,29 @@
+-- stack runghc src/Chapter9/DeleteToDo.hs
+
+module Chapter9.DeleteToDo where
+
+import System.IO
+import System.Directory
+import Data.List
+
+main :: IO ()
+main = do
+  let todoFile = "src/Chapter9/todo.txt"
+
+  contents <- readFile todoFile
+  let todoTasks = lines contents -- "aaa\nbbb" -> ["aaa", "bbb"]
+      numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+
+  putStrLn "These are your TO-DO items:"
+  mapM_ putStrLn numberedTasks
+  putStrLn "Which one do you want to delete?"
+
+  numberString <- getLine
+  let number = read numberString
+      newTodoItems = unlines $ delete (todoTasks !! number) todoTasks -- ["aaa", "bbb"] -> "aaa\nbbb\n"
+
+  (tempName, tempHandle) <- openTempFile "." "temp"
+  hPutStr tempHandle newTodoItems
+  hClose tempHandle
+  removeFile todoFile
+  renameFile tempName todoFile
