cd64fa92decc5d85eb0fa4cc285a76011571c183
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-26 16:06:03 +0900

    Add 10.1 演算子を追加しよう

--- test/Chapter10/Chapter10Spec.hs
+++ test/Chapter10/Chapter10Spec.hs
@@ -13,3 +13,11 @@ spec = do
             solveRPN "10 4 3 + 2 * -" `shouldBe` -4
         it "002" $
             solveRPN "2 3.5 +" `shouldBe` 5.5
+        it "/" $
+            solveRPN "4 2 /" `shouldBe` 2
+        it "^" $
+            solveRPN "4 2 ^" `shouldBe` 16
+        it "ln" $
+            solveRPN "1 2 3 ln" `shouldBe` 1.0986122886681098
+        it "sum" $
+            solveRPN "1 2 3 sum" `shouldBe` 6
