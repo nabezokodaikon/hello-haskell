658ec7605d83ce5a5a67cdb23bcdfc69f24ce747
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-30 15:49:11 +0900

    Add 12.1 newtypeをつかって型クラスのインスタンスを作る

--- src/Chapter12/Chapter12.hs
+++ src/Chapter12/Chapter12.hs
@@ -1,3 +1,11 @@
 -- stack runghc src/Chapter12/Chapter12.hs
 
 module Chapter12.Chapter12 where
+
+newtype CharList = CharList { getCharList :: [Char] }
+    deriving (Eq, Show)
+
+newtype Pair b a = Pair { getPair :: (a, b) }
+
+instance Functor (Pair c) where
+    fmap f (Pair (x, y)) = Pair (f x, y)
--- test/Chapter12/Chapter12Spec.hs
+++ test/Chapter12/Chapter12Spec.hs
@@ -1,11 +1,19 @@
 module Chapter12.Chapter12Spec (spec) where
 
 import Test.Hspec
+import Chapter12.Chapter12
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "" $ do
-        it "" $
+    describe "12.1 既存の方を新しい型にくるむ" $ do
+        it "CharList" $
+            CharList "abc" `shouldBe` CharList { getCharList = "abc" }
+        it "CharList 2" $
+            CharList "abc" == CharList "abc" `shouldBe` True
+        it "CharList 3" $
+            CharList "abc" == CharList "def" `shouldBe` False 
+        it "Pair" $
+            getPair (fmap (*100) (Pair (2, 3))) `shouldBe` (200, 3)
