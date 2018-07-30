ab36e8442fae3854b9afae489f5186b15d727520
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-07-31 00:11:19 +0900

    Add 12.3 リストはモノイド

--- test/Chapter12/Chapter12Spec.hs
+++ test/Chapter12/Chapter12Spec.hs
@@ -2,6 +2,7 @@ module Chapter12.Chapter12Spec (spec) where
 
 import Test.Hspec
 import Chapter12.Chapter12
+import Data.Monoid
 
 main :: IO ()
 main = hspec spec
@@ -17,3 +18,10 @@ spec = do
             CharList "abc" == CharList "def" `shouldBe` False 
         it "Pair" $
             getPair (fmap (*100) (Pair (2, 3))) `shouldBe` (200, 3)
+    describe "12.3 " $ do
+        it "リストはモノイド" $
+            [1, 2, 3] `mappend` [4, 5, 6] `shouldBe` [1, 2, 3, 4, 5, 6]
+        it "リストはモノイド 2" $
+            mconcat [[1, 2], [3, 6], [9]] `shouldBe` [1, 2, 3, 6, 9]
+        -- it "リストはモノイド 3" $
+            -- mempty :: [a] `shouldBe` []
